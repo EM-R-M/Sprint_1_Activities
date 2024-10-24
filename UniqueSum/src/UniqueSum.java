a4548ac01def795fe4aa81209b6d29e3b172c030
EM-R-M <emily.manton@aol.com>
2024-10-24 15:36:24 +0100

    errors fixed and commented

--- UniqueSum/src/UniqueSum.java
+++ UniqueSum/src/UniqueSum.java
@@ -1,8 +1,4 @@
-import java.sql.Array;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class UniqueSum {
 
@@ -17,23 +13,26 @@ public class UniqueSum {
 
         System.out.println("\nCALCULATING SUM OF THE NUMBERS");
 
-        // Get the unique values within the list to sum
-        List<Integer> uniqueList = intList.stream().distinct().toList();
-
         // Sum the unique values
         int sum = 0;
-        for (Integer integer : uniqueList) {
-            sum += integer;
+        for (Integer integer : intList) {
+            // Ensure that the number only exists once in the list
+            if (Collections.frequency(intList, integer) == 1) {
+                sum += integer;
+            }
         }
         // Display the sum of the unique values
         System.out.println("\nThe sum of the unique values is: " + sum);
 
     }
 
+    // Function to insure that the input is a valid integer
     private static int checkInteger(Scanner myObj){
         while (true) {
+            // Prompt the user to input an integer
             System.out.println("Please input an integer");
             String input = myObj.nextLine();
+            // If the value is an integer, then the try loop will succeed
             try {
                 return Integer.parseInt(input);
             }
