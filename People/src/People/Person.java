022d6f4d35839fb4a437cf5fc657a656d796235a
EM-R-M <emily.manton@aol.com>
2024-10-25 11:13:31 +0100

    Person task attempted

--- People/src/People/Person.java
+++ People/src/People/Person.java
@@ -2,12 +2,22 @@ package People;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Person {
 
     private String firstName;
     private String lastName;
     private LocalDate dateOfBirth;
+    private static int totalPeople = 0;
+
+    public Person(String firstName, String lastName, LocalDate dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        totalPeople++;
+    }
 
     public void setFirstName(String firstName) { this.firstName = firstName;  }
     public String getFirstName() { return firstName; }
@@ -18,14 +28,12 @@ public class Person {
     public void setDateOfBirth(LocalDate dateOfBirth) { this.dateOfBirth = dateOfBirth;  }
     public LocalDate getDateOfBirth() { return dateOfBirth; }
 
-    public void setPerson(String firstName, String lastName, LocalDate dateOfBirth){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.dateOfBirth = dateOfBirth;
-    }
-
     public int getAge() {
         return Period.between(dateOfBirth, LocalDate.now()).getYears();
+    }
+
+    public int getTotalPeople() {
+        return totalPeople;
     }
 
     public String getFormatted() {
