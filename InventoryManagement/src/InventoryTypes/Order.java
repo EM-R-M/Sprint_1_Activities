1a3f360d0521482cde57cb178f341eb20749f405
EM-R-M <emily.manton@aol.com>
2024-10-25 15:28:09 +0100

    Last edits before meeting

--- InventoryManagement/src/InventoryTypes/Order.java
+++ InventoryManagement/src/InventoryTypes/Order.java
@@ -1,4 +1,19 @@
 package InventoryTypes;
 
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 public class Order {
+
+    private static final ArrayList<Order> orders = new ArrayList<>();
+    private static int nextOrderID = 0;
+    private final int orderID;
+    private Customer customer;
+    private Dictionary<String, Integer> orderedProducts = new Hashtable<>();
+
+    public Order (){
+        orders.add(this);
+    }
+
 }
